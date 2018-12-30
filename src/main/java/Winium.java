000ed7465c40e4f0a4adf200a7693b59e9006669
SoniaWadhwani <sonirwadhwani@gmail.com>
2018-12-30 18:25:33 +0530

    First Commit by Sonia

---
+++ src/main/java/Winium.java
@@ -0,0 +1,30 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+
+public class Winium
+{
+	public static void main(String [] args) throws MalformedURLException, InterruptedException
+	{
+		DesktopOptions option = new DesktopOptions();
+		option.setApplicationPath("C:\\Windows\\System32\\calc.exe");
+		WiniumDriver driver = new WiniumDriver(new URL("http://localhost:9999"), option);
+		Thread.sleep(5000);
+		driver.findElement(By.name("7")).click();
+		driver.findElement(By.name("Add")).click();
+		driver.findElement(By.name("3")).click();
+		driver.findElement(By.name("Equals")).click();
+		Thread.sleep(5000);
+		//String Result = driver.findElement(By.id("CalculatorResults")).getAttribute("Name");
+		WebElement element = driver.findElement(By.id("158"));
+		boolean Result = element.isEnabled();
+		System.out.println(Result);
+		
+		
+				
+	}
+}
